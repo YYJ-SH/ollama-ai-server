9f98b09d7d6d54dad793a51983b8f9ce3fdfbdb3
YYJ-SH <20200936@sungshin.ac.kr>
2025-10-23 10:46:04 +0900

    found ollama runin in local ports. should change inner ports.

--- fastapi_app/app/config.py
+++ fastapi_app/app/config.py
@@ -4,9 +4,12 @@
 OLLAMA_ENDPOINTS = {
     "llama3:7b": "http://ollama_gpu0:11434",
     "qwen2.5vl:7b": "http://ollama_gpu0:11434",
+    "exaone3.5:7.8b": "http://ollama_gpu0:11434",
     "gpt-oss:20b": "http://ollama_gpu1:11434"
 }
 
+# 허용된 모델 목록
 SUPPORTED_MODELS = set(OLLAMA_ENDPOINTS.keys())
 
+# 데이터베이스 파일 위치
 DATABASE_FILE = "/app/database/api_server.db"
